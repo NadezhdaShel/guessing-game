e4999c0dec97dc9a1d52bc7d734acc1b3963e7c9
Nadia-website <nadia344@yandex.ru>
2023-01-31 18:56:27 +0300

    feat: solution task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor(number) {
+        this.number = number;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let p = Math.round((this.min + this.max) / 2);
+        if (this.number < p) {
+            return this.lower();
+        } else if (this.number > p) {
+            return this.greater();
+        } else return p;
     }
 
     lower() {
-
+        this.max = Math.round((this.min + this.max) / 2);
+        return this.guess();
     }
 
     greater() {
-
+        this.min = Math.round((this.min + this.max) / 2);
+        return this.guess();
     }
 }
 
